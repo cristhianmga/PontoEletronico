cfab51c5009602fcc4b7a73f9d3a858efbb4acd9
Cristhian de Almeida <cristhian.almeida@certsys.com.br>
2019-12-11 16:22:22 -0300

    Novas views e metodos na controller funcionario

--- PontoEletronico/Controllers/FuncionarioController.cs
+++ PontoEletronico/Controllers/FuncionarioController.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using PontoEletronico.Data;
+using PontoEletronico.Models;
+using PontoEletronico.Servico.Base;
 
 namespace PontoEletronico.Controllers
 {
     public class FuncionarioController : Controller
     {
+        private readonly BaseServico servico;
+        private readonly IMapper _mapper;
+        public FuncionarioController(ApplicationDbContext ctx, IMapper mapper)
+        {
+            servico = new BaseServico(ctx);
+            _mapper = mapper;
+        }
+            
         public IActionResult Index()
         {
             return View();
@@ -18,6 +30,31 @@ namespace PontoEletronico.Controllers
         public IActionResult AddFuncionarioEmpresa(int EmpresaId)
         {
             ViewBag.CpfEncontrado = false;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddFuncionarioNaoCadastrado(string cpf)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddFuncionarioEmpresa(string cpf)
+        {
+            var funcionario = servico.ObterTodos<Funcionario>().Where(x => x.Cpf == cpf);
+            if (funcionario.Count() > 1)
+            {
+                return View("AddFuncionarioNaoCadastrado");
+            }
+            else
+            {
+                return View("AddFuncionarioCadastrado");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddFuncionarioNaoCadastrado()
+        {
             return View();
         }
     }
